625a58807f9a835827fd9bc17b94e35d434c72a4
Elena Yevsyukova <elena.yevs@gmail.com>
2021-05-29 20:28:55 +0200

    Create an array.

--- src/main/java/com/nixsolutions/elenaevsiukova/hw2/Main.java
+++ src/main/java/com/nixsolutions/elenaevsiukova/hw2/Main.java
@@ -1,4 +1,16 @@
 package com.nixsolutions.elenaevsiukova.hw2;
 
 public class Main {
+    public static void main(String[] args) {
+        /**
+         * A part of the sea where the number of icebergs will be counted.
+         */
+        int[][] sea = {{1, 0, 0, 0, 0, 0},
+                {1, 1, 0, 0, 1, 0},
+                {0, 0, 0, 0, 0, 1},
+                {1, 1, 0, 0, 0, 0},
+                {1, 1, 1, 0, 1, 1}};
+
+
+    }
 }
