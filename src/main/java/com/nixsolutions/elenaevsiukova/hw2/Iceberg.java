b9d118f31d40aa4d38412600f4261ae667c8d0b3
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2021-05-31 23:38:23 +0300

    Grow array or rects

--- src/main/java/com/nixsolutions/elenaevsiukova/hw2/Iceberg.java
+++ src/main/java/com/nixsolutions/elenaevsiukova/hw2/Iceberg.java
@@ -1,5 +1,7 @@
 package com.nixsolutions.elenaevsiukova.hw2;
 
+import java.util.Arrays;
+
 /**
  * Айсберг состоит минимум из одного квадрата.
  *
@@ -7,15 +9,31 @@ package com.nixsolutions.elenaevsiukova.hw2;
  */
 final class Iceberg {
 
-    private Square[] squares = new Square[10];
+    /**
+     * Первоначальный размер массива квадратов и дельта приращения массива, при его наполнении.
+     */
+    private static final int GROWTH = 10;
+
+    private Square[] squares = new Square[GROWTH];
     private int count = 0;
 
     Iceberg(Square first) {
         add(first);
     }
 
+    private void add(Square square) {
+        if (count == squares.length) {
+            grow();
+        }
+        squares[count] = square;
+        count++;
+    }
 
-
+    private void grow() {
+        int newLength = squares.length + GROWTH;
+        Square[] newSquares = Arrays.copyOf(squares, newLength);
+        squares = newSquares;
+    }
 }
 
 
